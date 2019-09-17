d797590dd943690d496e8624a1450207562b27c3
tedliu-TW <m0461006@g2.usc.edu.tw>
2019-09-17 14:09:12 +0800

    第 1 章，第 24 單元 - Java8 新的日期時間處理(LocalDataTime)

--- main/java/com/ted1/parking/Tester.java
+++ main/java/com/ted1/parking/Tester.java
@@ -1,13 +1,45 @@
 package com.ted1.parking;
 
+import javafx.scene.input.DataFormat;
+
 import java.sql.SQLOutput;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) {
+        //ctrl+ alt + m 
+ //       Java();
+//        Java 8
+       Java8();
+
+
+    }
+
+    private static void Java8() {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        //Local
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(formatter.format(now));
+        now.plus(Duration.ofHours(3));
+        System.out.println(now.plus(Duration.ofHours(3)));
+
+
+        LocalDateTime other =
+                LocalDateTime.of(2018,11,23,8,0,0);
+        System.out.println(other);
+    }
+
+    private static void Java() {
         Date date = new Date();
         System.out.println(date);
         System.out.println(date.getTime());
@@ -31,11 +63,5 @@ public class Tester {
         System.out.println(calendar.getTime());
         calendar.add(calendar.DAY_OF_YEAR,3);
         System.out.println(calendar.getTime());
-
-
-
-
-
-
     }
 }
