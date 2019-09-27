b366d7d1a320a909920eb73feeb74b0c16b2e2fe
tedliu-TW <m0461006@g2.usc.edu.tw>
2019-09-27 20:21:06 +0800

    第 1 章，第 34 單元 - 解鎖一輸出入相關類別緩衝區(Buffered)讀取

--- main/java/com/ted1/io/Tester.java
+++ main/java/com/ted1/io/Tester.java
@@ -1,27 +1,76 @@
 package com.ted1.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
-        File file = new File("data.text");
+        //inputeream();
+        //filereader();
+        try {
+            BufferedReader br =new BufferedReader(new FileReader("data.txt"));
+            try {
+                String line = br.readLine();
+                while(line!=null){
+                    System.out.println(line);
+                    line=br.readLine();
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        };
+
+    }
+
+    private static void filereader() {
+        try {
+            FileReader fr = new FileReader("data.txt");
+            try {
+                int n  = fr.read();
+                while(n!=-1){
+                    System.out.print((char)n);
+                    n=fr.read();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputeream() {
+        File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
         //InputStream top io
         try {
             InputStream is = new FileInputStream(file);
+            int n = is.read();
+            while(n!=-1){
+                System.out.println((char)n);
+                n = is.read();
+            }
+
+
+
+//            System.out.println(n);
+//            System.out.println(is.read());
+//            System.out.println(is.read());
+//            System.out.println(is.read());
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
             System.out.println("message");
 
+        }catch (IOException e){
+            e.printStackTrace();
         }
 
-
-
+        System.out.println("File open success");
     }
 
 }
